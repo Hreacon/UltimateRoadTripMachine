572626ca4e8fd6b447d4e1f702faf4071d94bf9f
Michael Dada <mail.michaeldada@gmail.com>
2016-03-07 08:57:30 -0800

    add getters and setters for roadtrip class

--- Objects/RoadTrip.cs
+++ Objects/RoadTrip.cs
@@ -6,10 +6,54 @@ using JensenNS.Objects;
 
 namespace UltimateRoadTripMachineNS.Objects
 {
-  public class RoadTrip
+  public class RoadTrip 
   {
-    public RoadTrip()
-    {
+        private int _id;
+        private string _name;
+        private string _description;
+        public RoadTrip(string name, string description, int id = 0)
+    {  
+        _id = id;
+        _name = name;
+        _description = description;
     }
+    
+    public override bool Equals(System.Object otherTrip)
+    {
+        if (!(otherTrip is RoadTrip))
+        {
+            return false;
+        }
+        else{
+            RoadTrip newTrip = (RoadTrip) otherTrip;
+            bool IdEquality = this.GetId() == newTrip.GetId();
+            bool NameEquality = this.GetName() == newTrip.GetName();
+            bool DescriptionEquality = this.GetDescription() == newTrip.GetDescription();
+            
+            return (NameEquality && DescriptionEquality && IdEquality);
+        }
+    }
+    
+    public int GetId()
+    {
+        return _id;
+    }
+    public string GetName()
+    {
+        return _name;
+    }
+    public void SetName(string name)
+    {
+        _name = name;
+    }
+    public string GetDescription()
+    {
+        return _description;
+    }
+    public void SetDescription(string description)
+    {
+        _description = description;
+    }
+    
   } // end class
 } // end namespace
