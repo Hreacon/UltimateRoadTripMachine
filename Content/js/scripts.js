e6fa89eefc709567f99c2d982dde5b209e6cc071
chris berry <chrisberry321@yahoo.com>
2016-03-10 14:32:04 -0800

    added a sidepanel function

--- Content/js/scripts.js
+++ Content/js/scripts.js
@@ -100,3 +100,10 @@ $(document).ready(function() {
       $('.overlay iframe').css("pointer-events", "none");
     });
  });
+
+ $(document).ready(function(){
+  $('.sliding-panel-button,.sliding-panel-fade-screen,.sliding-panel-close').on('click touchstart',function (e) {
+    $('.sliding-panel-content,.sliding-panel-fade-screen').toggleClass('is-visible');
+    e.preventDefault();
+  });
+});
